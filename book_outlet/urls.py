a2aad2115127c4174f77c088823605122c0d2790
deepaksp24 <96986851+deepaksp24@users.noreply.github.com>
2024-08-18 10:44:44 +0530

    completed data & model

--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.index),
-    path("<int:id>", views.book_detail)
+    path("<slug:slug>", views.book_detail, name="book-detail")
 ]
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,22 +1,29 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from .models import Book
+from django.db.models import Avg, Max, Min
 
 # Create your views here.
 
 
 def index(request):
-    books = Book.objects.all()
-    return render(request, "book_outlet/index.html", {"books": books})
+    books = Book.objects.all().order_by("-title")
+    num_books = books.count()
+    avg_books = books.aggregate(Avg("rating"))
+    return render(request, "book_outlet/index.html", {
+        "books": books,
+        "total_number_of_books": num_books,
+        "avaerage_rating": avg_books
+    })
 
 
-def book_detail(request, id):
+def book_detail(request, slug):
     # try:
     #     book = Book.objects.get(pk=id)
     # except:
     #     raise Http404()
 
-    book = get_object_or_404(Book, pk=id)
+    book = get_object_or_404(Book, slug=slug)
     return render(request, "book_outlet/book_detail.html", {
         "title": book.title,
         "author": book.author,
