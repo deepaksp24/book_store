0a605cbaeb6642c9d85e6fc6e862c405f1aaa924
deepaksp24 <96986851+deepaksp24@users.noreply.github.com>
2024-08-19 10:41:11 +0530

    added adim page

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Book
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",)
+    list_filter = ("title", "author", "rating")
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title", "author", "rating")
+
+
+admin.site.register(Book, BookAdmin)
